8febc248d1726154b02c8cf205e3f2408ac1f6fa
swapnil20071441 <swapnilbhoite9810@gmail.com>
2026-08-19 09:25:50 +0530

    Add files via upload

---
+++ listt.py
@@ -0,0 +1,41 @@
+n=int(input("enter members of list:"))
+borrow =[0]*n
+
+for i in range(n):
+    borrow[i]=int(input("enter books borrowed by member"+str(i+1)+":"))
+
+total=0
+for i in range(n):
+    total =total+borrow[i]
+    average = total/n
+print("\naverage books borrowed:",average)
+
+highest= borrow[0]
+lowest = borrow[0]
+for i in range(1,n):
+    if borrow[i]>highest:
+        highest= borrow[i]
+    if borrow[i]<lowest:
+        lowest =borrow[i]
+print("Highest borrow count:",highest) 
+print("Lowest borrow count:",lowest)
+
+count=0
+for i in range(n):
+    if borrow[i]==0:
+        count=count+1
+print("\n Members who purchased no books :",count)    
+
+mode = borrow[0] 
+highest= 0
+for i in range(n):
+    c = 0    
+    for j in range(n):        
+        if borrow[i] == borrow[j]: 
+             c += 1
+
+    if c > highest:      
+          highest = c       
+          mode = borrow[i]
+
+print("Most frequently borrowed books:", mode)
